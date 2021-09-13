1c8da5070c3cc626db6c3f585626b1eba460d78b
Thejashwini Swamy (RBEI/ETP3) <Swamy.Thejashwini@in.bosch.com>
2021-09-13 11:54:54 +0530

    Retry request in 5 seconds on getting rate limit error from Bitbucket server

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
@@ -114,6 +114,8 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.ProtectedExternally;
 
 import static java.util.Objects.requireNonNull;
 
@@ -150,6 +152,8 @@ public class BitbucketServerAPIClient implements BitbucketApi {
 
     private static final String API_COMMIT_STATUS_PATH = "/rest/build-status/1.0/commits{/hash}";
     private static final Integer DEFAULT_PAGE_LIMIT = 200;
+    private static final int TOO_MANY_REQUESTS_HTTP_STATUS = 429;
+    private static final long REQUEST_WAIT_TIME_DEFAULT = 5000;
 
     /**
      * Repository owner.
@@ -848,8 +852,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
             authenticator.configureRequest(httpget);
         }
 
-        try(CloseableHttpClient client = getHttpClient(httpget);
-                CloseableHttpResponse response = client.execute(httpget, context)) {
+        try (CloseableHttpResponse response = executeMethod(httpget, context)) {
             String content;
             long len = response.getEntity().getContentLength();
             if (len == 0) {
@@ -891,8 +894,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
             authenticator.configureRequest(httpget);
         }
 
-        try (CloseableHttpClient client = getHttpClient(httpget);
-                CloseableHttpResponse response = client.execute(httpget, context)) {
+        try (CloseableHttpResponse response = executeMethod(httpget, context)) {
             BufferedImage content;
             long len = response.getEntity().getContentLength();
             if (len == 0) {
@@ -1001,8 +1003,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
             authenticator.configureRequest(httpget);
         }
 
-        try(CloseableHttpClient client = getHttpClient(httpget);
-                CloseableHttpResponse response = client.execute(httpget, context)) {
+        try(CloseableHttpResponse response = executeMethod(httpget, context)) {
             EntityUtils.consume(response.getEntity());
             return response.getStatusLine().getStatusCode();
         } finally {
@@ -1046,8 +1047,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
             authenticator.configureRequest(request);
         }
 
-        try(CloseableHttpClient client = getHttpClient(request);
-                CloseableHttpResponse response = client.execute(request, context)) {
+        try (CloseableHttpResponse response = executeMethod(request, context)) {
             if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NO_CONTENT) {
                 EntityUtils.consume(response.getEntity());
                 // 204, no content
@@ -1190,4 +1190,31 @@ public class BitbucketServerAPIClient implements BitbucketApi {
         }
         return content;
     }
+
+    @Restricted(ProtectedExternally.class)
+    protected CloseableHttpResponse executeMethod(HttpRequestBase httpMethod, HttpClientContext requestContext)
+            throws IOException {
+        CloseableHttpClient client = getHttpClient(httpMethod);
+        CloseableHttpResponse response = client.execute(httpMethod, requestContext);
+        while (response.getStatusLine().getStatusCode() == TOO_MANY_REQUESTS_HTTP_STATUS) {
+            response.close();
+            httpMethod.releaseConnection();
+
+            /*
+             * TODO: When bitbucket starts supporting rate limit expiration time, remove 5
+             * sec wait and put code to wait till expiration time is over. It should also
+             * fix the wait for ever loop.
+             */
+            LOGGER.fine("Bitbucket server API rate limit reached, sleeping for 5 sec then retry...");
+            try {
+                Thread.sleep(REQUEST_WAIT_TIME_DEFAULT);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            response = client.execute(httpMethod, requestContext);
+        }
+        return response;
+    }
+
 }
